cb25a1ff5b8a032f32eed24d7fa868dc1c723786
Ace <acekisch@gmail.com>
2023-11-21 17:54:22 -0800

    footer update

--- components/footer/index.js
+++ components/footer/index.js
@@ -5,11 +5,22 @@ export default function Footer() {
     <footer className="content-grid">
       <div className={styles.footer}>
 
+        {/* <div className={styles.newsletter}>
+        <form>
+          <label htmlFor="email">Sign up for our newsletter</label>
+          <div>
+          <input type="email" placeholder="Your email address" />
+          <button type="submit">Sign Up</button>
+          </div>
+        </form>
+      </div> */}
+
+        <p>Follow us</p>
         <div className={styles.icons}>
           <img src="/icons/treasure-chest-solid.svg" width="100%" title="InstaGold" />
           <img src="/icons/scroll-old-solid.svg" width="100%" title="FellowshipBook" />
           <img src="/icons/dragon-solid.svg" width="100%" title="xXTwitter for DragonsXx" />
-          {/* <img src="/icons/crystal-ball-solid.svg" width="100%" title="PalantIr" /> */}
+          <img src="/icons/crystal-ball-solid.svg" width="100%" title="Palantir" />
           <img src="/icons/dungeon-solid.svg" width="100%" title="MyDungeon" />
         </div>
 
