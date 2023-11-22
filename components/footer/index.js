4d46c0254833a4bc9b783287aff9d127d6df1922
Ace <acekisch@gmail.com>
2023-11-21 17:26:30 -0800

    Update index.js

--- components/footer/index.js
+++ components/footer/index.js
@@ -9,7 +9,8 @@ export default function Footer() {
           <img src="/icons/treasure-chest-solid.svg" width="100%" title="InstaGold" />
           <img src="/icons/scroll-old-solid.svg" width="100%" title="FellowshipBook" />
           <img src="/icons/dragon-solid.svg" width="100%" title="xXTwitter for DragonsXx" />
-          <img src="/icons/crystal-ball-solid.svg" width="100%" title="Palant-íri" />
+          {/* <img src="/icons/crystal-ball-solid.svg" width="100%" title="PalantIr" /> */}
+          <img src="/icons/dungeon-solid.svg" width="100%" title="MyDungeon" />
         </div>
 
         Copywrite &copy; 2023 The Shire
