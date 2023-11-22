3af3d175faafb4c10e3a48aa205904c97822e584
Ace <acekisch@gmail.com>
2023-11-21 17:06:40 -0800

    Update index.js

--- components/packaging/index.js
+++ components/packaging/index.js
@@ -9,7 +9,7 @@ export default function Packaging() {
             <strong>Mallorn Leaf Bags:</strong> Made from the resilient leaves of Mallorn trees, our organic bags naturally decompose leaving no environmental trace.
           </li>
           <li>
-            <strong>Elven Crafted Jars</strong> Masterfully crafted by Elves, our jars embody the elegance and precision of Rivendell's finest artisans. Made with ethereal yet durable materials, they ensure the freshness and purity of their contents, much like the lasting beauty of Lothlórien.
+            <strong>Elven Crafted Jars</strong> Masterfully crafted by Elves, our jars embody the elegance and precision of Rivendell&apos;s finest artisans. Made with ethereal yet durable materials, they ensure the freshness and purity of their contents, much like the lasting beauty of Lothlórien.
           </li>
           <li>
             <strong>Dwraven Crafted Barrels</strong> Our barrels are the same used for the finest dwarven ales. They are sturdy and reusable, perfect for storing your pipe-weed or other herbs.
