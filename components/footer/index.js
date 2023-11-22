1010a5ca6410b10e84e343baab466810a0ce5f07
Ace <acekisch@gmail.com>
2023-11-21 17:18:50 -0800

    Update index.js

--- components/footer/index.js
+++ components/footer/index.js
@@ -6,10 +6,10 @@ export default function Footer() {
       <div className={styles.footer}>
 
         <div className={styles.icons}>
-          <img src="/icons/dragon-solid.svg" width="100%" alt="xXTwitter for DragonsXx" />
-          <img src="/icons/treasure-chest-solid.svg" width="100%" alt="InstaGold" />
-          <img src="/icons/scroll-old-solid.svg" width="100%" alt="FellowshipBook" />
-          <img src="/icons/crystal-ball-solid.svg" width="100%" alt="Palant-íri" />
+          <img src="/icons/treasure-chest-solid.svg" width="100%" title="InstaGold" />
+          <img src="/icons/scroll-old-solid.svg" width="100%" title="FellowshipBook" />
+          <img src="/icons/dragon-solid.svg" width="100%" title="xXTwitter for DragonsXx" />
+          <img src="/icons/crystal-ball-solid.svg" width="100%" title="Palant-íri" />
         </div>
 
         Copywrite &copy; 2023 The Shire
