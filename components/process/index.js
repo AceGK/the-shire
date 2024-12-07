f06d5b5d0b107dd3090ad852b4758a76963c66e7
Ace <acekisch@gmail.com>
2024-12-07 14:46:25 -0800

    Update index.js

--- components/process/index.js
+++ components/process/index.js
@@ -8,9 +8,7 @@ export default function About() {
         <div>
           <h2>Quality</h2>
           <p>
-            Quality for us is as essential as a hearty meal. Each leaf and bud is
-            thoroughly inspected with care to ensure our strains are as pure and
-            wholesome as a Hobbit's pantry.
+            Quality for us is as essential as a hearty meal. Each leaf and bud is thoroughly inspected with care to ensure our strains are as pure and wholesome as a Hobbit&apos;s pantry.
           </p>
         </div>
       </div>
@@ -19,9 +17,7 @@ export default function About() {
         <div>
           <h2>Organic</h2>
           <p>
-            Our organic practices ensure that our strains are as natural as the
-            rolling hills of the Shire. We use no pesticides, chemicals, or magic
-            to grow our plants.
+            Our organic practices ensure that our strains are as natural as the rolling hills of the Shire. We use no pesticides, chemicals, or magic to grow our plants.
           </p>
         </div>
       </div>
@@ -30,9 +26,7 @@ export default function About() {
         <div>
           <h2>Sustainable</h2>
           <p>
-            Inspired by the lush greenery of the Shire, our farm is a bastion of
-            sustainable practices. We ensure that our footprint is as light as a
-            Hobbit's.
+            Inspired by the lush greenery of the Shire, our farm is a bastion of sustainable practices. We ensure that our footprint is as light as a Hobbit&apos;s.
           </p>
         </div>
       </div>
